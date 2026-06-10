a88039674eed6bef7e29dd0b3f386beca8b54f59
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-06-10 21:51:40 +0530

    Add api-route Next.js application

---
+++ api-route/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default  function Home() {
+  const handleClick=async ()=>{
+    let a = await fetch("api/add")
+    let res = await a.json()
+    console.log(res)
+
+  }
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+     <div>
+      <h1 className="text-xl font-bold">Next.js Api route demo</h1>
+      <button onClick={handleClick}></button>
+     </div>
+    </div>
+  );
+}

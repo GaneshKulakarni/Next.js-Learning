cea8eb3861c2ade331bbe7d4929880d4b804b6f0
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-06-11 14:15:29 +0530

    fineshed api-route

--- api-route/app/api/add/route.js
+++ api-route/app/api/add/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request){
-    return NextResponse.json({success:true,data:"yes"})
-
+    let data = await request.json();
+    console.log("the data is",data)
+    return NextResponse.json({success:true,data})
 }
--- api-route/app/page.js
+++ api-route/app/page.js
@@ -1,8 +1,17 @@
+"use client";
+
 import Image from "next/image";
 
 export default  function Home() {
   const handleClick=async ()=>{
-    let a = await fetch("api/add")
+    let data ={
+      name:"ganesh",
+      role:"engineer"
+    }
+    let a = await fetch("/api/add",{method:"POST",headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify(data),})
     let res = await a.json()
     console.log(res)
 
@@ -11,7 +20,7 @@ export default  function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <div>
       <h1 className="text-xl font-bold">Next.js Api route demo</h1>
-      <button onClick={handleClick}></button>
+      <button onClick={handleClick}>click me</button>
      </div>
     </div>
   );

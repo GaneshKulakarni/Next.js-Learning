b6e7cc31550d19b17debcdd5266450e341704612
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-06-27 15:44:47 +0530

    started new project

---
+++ project-1/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-white  h-15 px-5 items-center flex justify-center'>
+      <p className='text-center'>All Rights are reserved &copy; Getme A Chai Website</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ project-1/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-black text-white flex justify-between h-15 px-5 items-center'>
+            <div className="logo font-bold">GetMeAchain!</div>
+            <ul className='flex justify-between gap-4 '>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Signups</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
